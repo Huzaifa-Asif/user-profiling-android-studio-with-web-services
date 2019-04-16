47b6806f4bf0a2131830063e8751ef4b555e7e0a
huzaifa85801 <huzaifa8580@gmail.com>
2019-04-17 01:45:57 +0500

    first commit

---
+++ app/src/main/java/com/theaweb/net/URLs.java
@@ -0,0 +1,14 @@
+package com.theaweb.net;
+
+
+public class URLs {
+
+    private static final String ROOT_URL = "http://theaweb.net/userprofiling/Api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+
+    public static final String URL_Login_account = "http://theaweb.net/userprofiling/login.php";
+    public static final String URL_create_account = "http://theaweb.net/userprofiling/register.php";
+
+}
